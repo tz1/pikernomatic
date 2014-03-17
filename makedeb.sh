85b1125c6d5329fdc2dd4175a52b7a54e2b2dbe2
tz1 <tz1@github.com>
2014-03-16 20:01:32 -0700

    some fixes for makedeb - still need to fix replaced files

--- makedeb.sh
+++ makedeb.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 source ./setvars.sh
 cd $BUILDOUT
+#FATFS doesn'tn kow about perms
+chmod 755 boot/*
+sudo chown root:root boot lib -R
 cp -a ../../control .
 echo 2.0 >debian-binary
 tar cvzf data.tar.gz boot lib
 md5sum `find boot lib -type f` >control/md5sums
-tar cvzf control.tar.gz control
+cd control
+tar cvzf ../control.tar.gz *
+cd ..
 ar rv ../../newkernl.deb debian-binary control.tar.gz data.tar.gz

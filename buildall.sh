434a3df71cd2293bc9efba71576ffe645add05dc
tz1 <tz1@github.com>
2014-03-17 17:17:14 -0700

    make sure we compile (mainly spi_bcm2708.ko, dma)

--- buildall.sh
+++ buildall.sh
@@ -6,6 +6,7 @@ export CROSS_COMPILE=$CCPREFIX
 export INSTALL_MOD_PATH=$BUILDOUT
 
 cd $KERNEL_SRC
+
 make oldconfig
 make -j 5
 make modules_install
@@ -14,11 +15,13 @@ gzip -9f <System.map >$BUILDOUT/boot/newkernl-$VERNAME.System.map.gz
 
 # Build the auxillary modules
 if [ x != x$CAN ] ; then
-    make KDIR=$KERNEL_SRC -C $BASEDIR/mcp2515async install
+    make KDIR=$KERNEL_SRC -C $BASEDIR/mcp2515async all install
 fi
-make KDIR=$KERNEL_SRC -C $BASEDIR/spi-config install
+
+make KDIR=$KERNEL_SRC -C $BASEDIR/spi-config all install
+
 if [ x != x$DMASPI ] ; then
 #replace
     find $BUILDOUT/lib/modules -name spi-bcm2708.ko -exec rm {} \;
-    make KDIR=$KERNEL_SRC -C $BASEDIR/spi-bcm2708 install
+    make KDIR=$KERNEL_SRC -C $BASEDIR/spi-bcm2708 all install
 fi
